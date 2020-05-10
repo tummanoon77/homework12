94a23d2e9c6483bce6efbbd169e48147aa3576de
Tummanoon Sripechpong <62575162+tummanoon77@users.noreply.github.com>
2020-05-10 15:29:08 -0400

    add some on seed.sql

--- seed.sql
+++ seed.sql
@@ -12,6 +12,19 @@ CREATE TABLE department(
 CREATE TABLE role(
   id INTEGER(11) NOT NULL AUTO_INCREMENT, 
   title VARCHA(30) NULL,
-  
+  yearly_salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES department(DepId)
+  PRIMARY KEY (id)
+)
+
+CREATE TABLE employee(
+  id INTEGER(11) NOT NULL AUTO_INCREMENT, 
+  first_name(30) NULL,
+  last_name(30) NULL,
+  role_id INT NOT NULL,
+  manager_id INT,
+  FOREIGN KEY (role_id) REFERENCES role(roleId)
+  -- FOREIGN KEY (manager_id) REFERANCES role(roleId)
   PRIMARY KEY (id)
 )
