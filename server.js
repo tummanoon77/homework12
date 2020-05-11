8e7da043ffe02f7b17b19ba7aa938281a874dcda
Tummanoon Sripechpong <62575162+tummanoon77@users.noreply.github.com>
2020-05-11 18:19:00 -0400

    add some prompt

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const mysql = require("mysql");
+const inquirer = require("inquirer");
+const table = require("console.table");
+
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -11,5 +14,102 @@ const connection = mysql.createConnection({
 
   // Your password
   password: "AuraZaira77",
-  database: "greatbayDB"
+  database: "employee_tracker"
 });
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  start();
+
+});
+
+  function start() {
+
+    inquirer.prompt([
+      {
+        type: "list",
+        name: "userChoice",
+        message: "What would you like to do?",
+        choices: [
+          "add Employee",
+          "view Employee",
+          "remove Employee"
+        ]
+      }
+    ]).then(userChoice => {
+      switch(userChoice.memberChoice) {
+      case "add Employee":
+        addEmployee();
+        break;
+      case "view Employee":
+        viewEmployee();
+        break;
+      case "remove Employee":
+          removeEmployee();
+        break;  
+       }
+    });
+  }
+  function addEmployee() {
+    inquirer.prompt([
+      {
+        type: "input",
+        name: "engineerName",
+        message: "What is your engineer's name?",
+        validate: answer => {
+          if (answer !== "") {
+            return true;
+          }
+          return "Please enter at least one character.";
+        }
+      },
+      {
+        type: "input",
+        name: "engineerId",
+        message: "What is your engineer's id?",
+        validate: answer => {
+          const pass = answer.match(
+            /^[1-9]\d*$/
+          );
+          if (pass) {
+            if (idArray.includes(answer)) {
+              return "This ID is already taken. Please enter a different number.";
+            } else {
+              return true;
+            }
+                        
+          }
+          return "Please enter a positive number greater than zero.";
+        }
+      },
+      {
+        type: "input",
+        name: "engineerEmail",
+        message: "What is your engineer's email?",
+        validate: answer => {
+          const pass = answer.match(
+            /\S+@\S+\.\S+/
+          );
+          if (pass) {
+            return true;
+          }
+          return "Please enter a valid email address.";
+        }
+      },
+      {
+        type: "input",
+        name: "engineerGithub",
+        message: "What is your engineer's GitHub username?",
+        validate: answer => {
+          if (answer !== "") {
+            return true;
+          }
+          return "Please enter at least one character.";
+        }
+      }
+    ]).then(answers => {
+      const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub);
+      teamMembers.push(engineer);
+      idArray.push(answers.engineerId);
+    });
+  }
